ffc396863556e4b3c19533fac3e7bba56b74f6b4
jeacar1323 <jeancar1323@gmail.com>
2023-05-10 21:43:51 -0300

    10.2

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,9 +9,4 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
-
-  @Get('leticia')
-  getLet(): string {
-    return this.appService.getLet();
-  }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -5,8 +5,4 @@ export class AppService {
   getHello(): string {
     return 'Hello World!';
   }
-
-  getLet(): string {
-    return 'Coisa mais linda desse mundo, que faz meu coração disparar só de pensar, que desde que conheci me faz me apaixonar cada vez mais, que é essa pessoa incrível e espetacular, que desde que apareceu em minha vida fez ela melhorar incrivelmente';
-  }
 }
